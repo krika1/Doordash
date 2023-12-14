3389a260615fe0c7d60975027250885bf273a77f
Kristian Budov <kristian.budov@ad.mentormate.bg>
2023-12-14 14:41:37 +0200

    changes

--- Doordash.API/Doordash.API/Controllers/ResturantController.cs
+++ Doordash.API/Doordash.API/Controllers/ResturantController.cs
@@ -122,7 +122,7 @@ namespace Doordash.API.Controllers
         }
 
         [HttpDelete, Route("resturants/{resturantId}")]
-        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
